ac53d0ab1f925579486adced98202c3119245e07
Deepali Futane <futanedeepali@gmail.com>
2020-05-01 16:10:45 +0100

    Update missingDuplicateIntegers.js

--- missingDuplicateIntegers.js
+++ missingDuplicateIntegers.js
@@ -1,28 +1,35 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
+//Codility demo test Javascript solution
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+// Given A = [1, 2, 3], the function should return 4.
+
+// Given A = [−1, −3], the function should return 1.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
+    let map = []
     
-    A.sort();
-    var newArr = [];
-    newArr.push(A[0])
-    if(A[A.length-1] > 1000000 || A[A.length-1]<0){
-        return 1;
-    }else{
-        for(var i=0; i<A.length; i++){
-            if(newArr[newArr.length-1] != A[i] && A[i] > 0){
-                newArr.push(A[i])
-            }
-        }
-        for(var i=0; i<newArr.length; i++){
-         if(newArr[i]+1 != newArr[i+1]){
-             return newArr[i]+1;
-         }
-        }
-        if(newArr[newArr.length-1] == newArr[newArr.length-2]+1){
-            return newArr[newArr.length-1]+1;
+    for (let i = 0; i < A.length; i++) {
+        if (0 <= A[i]) {
+            map[A[i]] = true
         }
     }
-    return 1;
+    
+    for (let i = 1; i <= map.length; i++) {
+        if (map[i] === undefined) {
+            return i
+        }
+    }   
+    return 1
 }
